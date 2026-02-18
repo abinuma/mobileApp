bd971a158ab3aa393a2306e0449f46ba46448ff6
abinu12 <abinumathewos@gmail.com>
2026-02-18 11:41:08 +0300

    added orders functionality for admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -47,7 +47,13 @@ const placeOrderRazorpay = async (req,res) => {
 
 // All orders data for admin panel
 const allOrders = async (req,res) => {
-    
+    try {
+        const orders = await orderModel.find({});
+        res.json({success: true, orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 // user order data for frontend
 const userOrders = async (req,res) => {
@@ -64,7 +70,14 @@ const userOrders = async (req,res) => {
 
 //update order status from admin panel
 const updateStatus = async (req,res) => {
-    
+    try {
+        const {orderId, status} = req.body;
+        await orderModel.findByIdAndUpdate(orderId, {status})
+        res.json({success: true, message: 'Status Updated'})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
