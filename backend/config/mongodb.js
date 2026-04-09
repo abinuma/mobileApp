2c751b440bbf5d4c5b25141a90530ac9ab0ac43b
abinu12 <abinumathewos@gmail.com>
2026-04-09 22:06:49 +0300

    fixed db

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     mongoose.connection.on('connected', () => {
-        console.log(`DB Connected! Database: ${mongoose.connection.name} Host: ${mongoose.connection.host}`);
+        console.log('DB Connected')
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce` )
+    await mongoose.connect(`${process.env.MONGODB_URI}/mobileAppEcommerce` )
 }
 
 export default connectDB;
